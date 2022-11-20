5b5e4cda34bade9c5986ecc266855046b738fce9
Viraj Shetty <viraj713@gmail.com>
2022-11-19 20:26:22 -0600

    Update streamlit_app.py
    added 4 lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@ import streamlit
 
 streamlit.title('Hello World')
 streamlit.header('This is my first streamlit program')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
